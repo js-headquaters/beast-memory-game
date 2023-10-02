e6e8a3d386cb18c690718281ba2726652f5afcb3
Konstantin Poleshchuk <javascriptoverlord@gmail.com>
2023-10-02 20:10:13 +0400

    fix wrong signal usage

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -36,7 +36,7 @@ export function GameComponent() {
 
           {!isRunningInTelegram() && (
             <button onClick={toggleMenu}>
-              {isMenuOpen ? "Close" : "Menu"}
+              {isMenuOpen.value ? "Close" : "Menu"}
             </button>
           )}
         </div>
