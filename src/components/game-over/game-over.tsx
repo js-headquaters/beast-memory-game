7b2055184a34638f08de859ac72c261d119c867f
Konstantin Poleshchuk <javascriptoverlord@gmail.com>
2023-10-08 01:29:09 +0400

    removed statistic from game over screen

--- src/components/game-over/game-over.tsx
+++ src/components/game-over/game-over.tsx
@@ -1,8 +1,5 @@
-import { KeyValueComponent } from "@components/shared/key-value-list/key-value";
-import { KeyValueListComponent } from "@components/shared/key-value-list/key-value-list";
 import { ModalComponent } from "@components/shared/modal/modal";
-import { PanelComponent } from "@components/shared/panel/panel";
-import { GameStateContext, StatisticContext } from "@interfaces/context";
+import { GameStateContext } from "@interfaces/context";
 import { cardMidResImageMap } from "@utils/card-image.utils";
 import { getAnimalPhrase, getRandomCongratulation } from "@utils/text.utils";
 import { useContext, useRef } from "preact/compat";
@@ -10,15 +7,10 @@ import "./game-over.css";
 
 /**
  * GameOverComponent is presented to the user upon the completion of a game. It displays
- * a congratulatory message and provides statistics from the completed level. Additionally,
- * average values are shown, enabling the user to gauge their performance and assess their
- * achievements.
+ * a congratulatory message and shows last played card in hig resolution, so user can see more card details.
  */
 export function GameOverComponent() {
-  const { timeSpentInSeconds, cardsFlipCount, lastOpenedCardType } =
-    useContext(GameStateContext);
-  const { averageCardFlipsCount, averageTimeSpentInSeconds } =
-    useContext(StatisticContext);
+  const { lastOpenedCardType } = useContext(GameStateContext);
 
   const headerMessage = useRef(getRandomCongratulation());
   const lastCardType = cardMidResImageMap.get(lastOpenedCardType.value);
@@ -35,29 +27,6 @@ export function GameOverComponent() {
         />
       </div>
       <div className="game-over__animal-phrase">{encouragingPhrase}</div>
-
-      <PanelComponent title="Time spent in seconds">
-        <KeyValueListComponent>
-          <KeyValueComponent
-            name="This game:"
-            value={timeSpentInSeconds.value}
-          />
-          <KeyValueComponent
-            name="Your average:"
-            value={averageTimeSpentInSeconds.value}
-          />
-        </KeyValueListComponent>
-      </PanelComponent>
-
-      <PanelComponent title="Card flips count">
-        <KeyValueListComponent>
-          <KeyValueComponent name="This game:" value={cardsFlipCount.value} />
-          <KeyValueComponent
-            name="Your average:"
-            value={averageCardFlipsCount.value}
-          />
-        </KeyValueListComponent>
-      </PanelComponent>
     </ModalComponent>
   );
 }
